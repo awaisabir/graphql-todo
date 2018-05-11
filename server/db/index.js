a9e3eb9386b91bb869527ee01151c1c5f43e9f4c
Theophilus JA <theophilus.ja@gmail.com>
2018-05-11 12:10:22 -0400

    Removed console logs for debugging

--- server/db/index.js
+++ server/db/index.js
@@ -5,9 +5,6 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, pr
     //logging: false
 });
 
-console.log(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD);
-
-
 const models = {
     User: sequelize.import('./models/User.js'),
     Todo: sequelize.import('./models/Todo.js')
