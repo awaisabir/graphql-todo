9c47d8395003139f29a389f2b06539c499db3ad6
awaisabir <awaisabirqureshi@gmail.com>
2018-05-14 12:30:03 -0400

    added auth mutations for apollo

--- client/src/apollo/auth.js
+++ client/src/apollo/auth.js
@@ -1,1 +1,20 @@
-// auth mutation
+import gql from 'graphql-tag';
+
+export const register = gql`
+  mutation register($username: String!, $password: String!) {
+    register(username: $username, password: $password) {
+      success
+      message
+    }
+  }
+`;
+
+export const login = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      success
+      message
+      token
+    }
+  }
+`;
