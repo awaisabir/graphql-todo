4c0938cc16846b4f18d1e55ab3e6dfd6b763303f
theoJA <theophilus.ja@gmail.com>
2018-05-15 02:30:31 -0400

    Added a model function to save user

--- server/db/models/User.js
+++ server/db/models/User.js
@@ -1,3 +1,5 @@
+import bcrypt from 'bcryptjs';
+
 export default (sequelize, DataTypes) => {
     const User = sequelize.define('user', {
         username: {
@@ -14,6 +16,19 @@ export default (sequelize, DataTypes) => {
     User.associate = models => {
         User.hasMany(models.Todo);
     }
+
+    User.saveUser = user => (
+        new Promise (async (resolve, reject) => {
+            try {
+                const salt    = await bcrypt.genSalt(15);
+                user.password = await bcrypt.hash(user.password, salt);
+                const newUser = await User.create(user);
+                resolve(newUser);
+            } catch(err) {
+                reject(err);
+            }
+        })
+    )
 
     return User;
 };
