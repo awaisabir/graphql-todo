b09cf1c481b4a405ac75d328450817b7535f5875
awaisabir <awaisabirqureshi@gmail.com>
2018-05-15 22:57:22 -0400

    fixed apollo query name

--- client/src/apollo/users.js
+++ client/src/apollo/users.js
@@ -1,13 +1,13 @@
 import gql from 'graphql-tag';
 
-export const fetchUserById = gql`
-query user($id: Int!) {
-  user(id: $id) {
+export const getUser = gql`
+query getUser($id: Int!) {
+  getUser(id: $id) {
     id
     username
     todos {
       id
-      heading
+      title
       completed
     }
   }
