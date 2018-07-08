d496cb4a359e7d3c9c6a484ce173e5ef3e166552
awaisabir <aqureshi@klipfolio.com>
2018-07-08 14:01:34 -0400

    Added stuff to change the state based on token:

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import decode from 'jwt-decode';
+import jwt from 'jsonwebtoken';
 
-import { BrowserRouter as Router, Route  } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import RouterRoot from '../components/routing/RouterRoot';
 import NavBar from '../components/Navbar';
+import config from '../config/config.js';
 
 class App extends Component {
   constructor() {
@@ -14,36 +17,56 @@ class App extends Component {
     };
 
     this.onLogout = this.onLogout.bind(this);
-    this.onLogin  = this.onLogin.bind(this);
+    this.onLogin = this.onLogin.bind(this);
+  }
+
+  componentDidMount() {
+    const token = localStorage.getItem('token');
+    try {
+      const result = token && jwt.verify(token, config.key);
+      if (!result) {
+        this.setState({ isLoggedIn: false, token: '' });
+      } else {
+        this.setState({ isLoggedIn: true, token });
+      }
+    } catch (jsonWebTokenError) {
+      localStorage.clearItem('token');
+      this.setState({ isLoggedIn: false, token: '' });
+    }
   }
 
   onLogout() {
     localStorage.clear();
-    this.setState({isLoggedIn: false, token: ''});
+    this.setState({ isLoggedIn: false, token: '' });
   }
 
   onLogin(token) {
     localStorage.setItem('token', token);
-    this.setState({token, isLoggedIn: true});
+    this.setState({ token, isLoggedIn: true });
   }
 
   render() {
     return (
       <Router>
         <div>
-          <NavBar isLoggedIn={this.state.isLoggedIn} onLogout={this.onLogout}/>
-          <div className='container'>
+          <NavBar isLoggedIn={this.state.isLoggedIn} onLogout={this.onLogout} />
+          <div className="container">
             <div>
-              <Route path='/' render={() => <RouterRoot 
-                isLoggedIn={this.state.isLoggedIn} 
-                onLogin={this.onLogin}
-              />} />
+              <Route
+                path="/"
+                render={() => (
+                  <RouterRoot
+                    isLoggedIn={this.state.isLoggedIn}
+                    onLogin={this.onLogin}
+                  />
+                )}
+              />
             </div>
           </div>
         </div>
       </Router>
     );
   }
-};
+}
 
 export default App;
