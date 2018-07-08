7ada6fc0545be32e2f84c8143cc7c357ef29b01e
awaisabir <aqureshi@klipfolio.com>
2018-07-08 14:00:11 -0400

    Added routing changes:

--- client/src/components/routing/RouterRoot.js
+++ client/src/components/routing/RouterRoot.js
@@ -5,14 +5,21 @@ import Login from '../../containers/Login';
 import Register from '../../containers/Register';
 import Profile from '../../containers/Profile';
 
-export default ({isLoggedIn, onLogin}) => (
+export default ({ isLoggedIn, onLogin }) => (
   <Switch>
-    <Route exact path='/login' render={ props => <Login {...props} onLogin={onLogin}/> } />
-    <Route exact path='/register' component={Register} />
-    
-    {isLoggedIn ? 
-      <Route exact path='/profile' component={Profile} />
-      : <Redirect to='/login' />
+    {isLoggedIn
+      ? <Route exact path="/login" render={props => <Redirect to="/profile" />} />
+      : <Route exact path="/login" render={props => <Login {...props} onLogin={onLogin} />} />
+    }
+
+    {isLoggedIn
+      ? <Route exact path="/register" render={props => <Profile />} />
+      : <Route exact path="/register" component={Register} />
+    }
+
+    {isLoggedIn
+      ? <Route exact path="/profile" component={Profile} />
+      : <Redirect to="/login" />
     }
   </Switch>
 );
